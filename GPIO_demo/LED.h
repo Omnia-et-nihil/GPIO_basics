69a125aeeb64fb2537bbae8121f793b4dc816cb7
Omnia-et-nihil <113312458+Omnia-et-nihil@users.noreply.github.com>
2024-12-23 17:43:42 -0800

    Update LED.h

--- GPIO_demo/LED.h
+++ GPIO_demo/LED.h
@@ -8,18 +8,22 @@ class LED{
   public:
     LED();
     void begin(byte pin);
+    bool state;
     void on();
     void off();
     void toggle();
-    bool status();
     void blink(unsigned long spacing);
     void blink(unsigned long timeOn, unsigned long timeOff);
     void pwm(byte val);
+    byte breathe(unsigned long breatheRate);
+    bool pulseRunning;
+    void pulse(bool &pulseBegin, unsigned long pulseDuration);
+    bool patternRunning;
+    void pattern(bool &patternBegin, unsigned int ptrn[]);
 
 
     private:
     byte _pin;
-    bool _state;
     unsigned long _lastOn;
     unsigned long _lastOff;
 
