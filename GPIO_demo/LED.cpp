ed8654fd8cd1a66966eaf6cc10ce5e1a5f62e109
Omnia-et-nihil <113312458+Omnia-et-nihil@users.noreply.github.com>
2024-12-23 17:43:28 -0800

    Update LED.cpp

--- GPIO_demo/LED.cpp
+++ GPIO_demo/LED.cpp
@@ -7,30 +7,28 @@ void LED::begin(byte pin){
   _pin = pin;
   pinMode(_pin, OUTPUT);
   digitalWrite(_pin, LOW);
-  _state = false;
+  state = false;
+  pulseRunning = false;
+  patternRunning = false;
 }
 
 void LED::on(){
   digitalWrite(_pin, HIGH);
-  _state = true;
+  state = true;
 }
 
 void LED::off(){
   digitalWrite(_pin, LOW);
-  _state = false;
+  state = false;
 }
 
 void LED::toggle(){
-  digitalWrite(_pin, !_state);
-  _state = !_state;
-}
-
-bool LED::status(){
-  return _state;
+  digitalWrite(_pin, !state);
+  state = !state;
 }
 
 void LED::blink(unsigned long spacing){
-  if(_state){
+  if(state){
     if(millis() - _lastOn > spacing){
       off();
       _lastOff = millis();
@@ -44,7 +42,7 @@ void LED::blink(unsigned long spacing){
 }
 
 void LED::blink(unsigned long timeOn, unsigned long timeOff){
-  if(_state){
+  if(state){
     if(millis() - _lastOn > timeOn){
       off();
       _lastOff = millis();
@@ -61,6 +59,63 @@ void LED::pwm(byte val){
   analogWrite(_pin, val);
 }
 
+
+byte LED::breathe(unsigned long breatheRate){
+  static int breatheDirection;
+  static byte breatheVal;
+  static unsigned long breatheTime;
+  if(millis() - breatheTime > breatheRate){
+    if(breatheVal == 255){
+      breatheDirection = -1;
+    }else if(breatheVal == 0){
+      breatheDirection = 1;
+    }
+    breatheVal += breatheDirection;
+    pwm(breatheVal);
+    breatheTime -= millis();
+    return breatheVal;
+  }
+}
+      
+void LED::pulse(bool &pulseBegin, unsigned long pulseDuration){
+  static unsigned long pulseStart;
+  if(pulseBegin){
+    on();
+    pulseStart = millis();
+    pulseBegin = false;
+    pulseRunning = true;
+  }
+  if(pulseRunning){
+    if(millis - pulseStart > pulseDuration){
+      off();
+      pulseRunning = false;
+    }
+  }
+}
+
+void LED::pattern(bool &patternBegin, unsigned int ptrn[]){
+  static unsigned int count;
+  static unsigned long lastChange;
+
+  if(patternBegin){
+    count = 0;
+    on();
+    lastChange = millis();
+    patternBegin = false;
+    patternRunning = true;
+  }
+  if(patternRunning){
+    if(millis() - lastChange > ptrn[count]){
+      toggle();
+      lastChange = millis();
+      count++;
+      if(ptrn[count] == 0){
+        off();
+        patternRunning = 0;
+      }
+    }
+  }
+}
 
 
 
